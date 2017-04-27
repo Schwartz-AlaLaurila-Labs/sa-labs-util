018b1b6187d867ba6f86443468a0c0ca0133d473
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2017-04-27 20:30:25 +0300

    moved getPSTH to spike time feature

--- src/main/matlab/+sa_labs/+common/+spike/+features/SpikeTimeFeature.m
+++ src/main/matlab/+sa_labs/+common/+spike/+features/SpikeTimeFeature.m
@@ -1,30 +1,39 @@
-classdef SpikeTimeFeature < sa_labs.analysis.entity.Feature
+classdef SpikeTimeFeature < handle
     
     
-    % Time axis :
+    % Figure(1) Time axis :
     %
     %       { baselineIntervalLen }                  {---    responseIntervalLength --}  {-- poststimIntervalLen --}
     %       |----------------------|-----------------|------------------|---------------|---------------------------|
-    %      0.5                  <desc>             0.0                <desc>
-    % (baselineStart)       (baseLineEnd = 0) (intervalStart = 0) (endOffset = 0) (intervalEnd)                   (end)
+    %     -0.5                  <desc>             0.0                <desc>
+    % (baselineStart)       (baseLineEnd = 0) (intervalStart = 0) (endOffset = 0) (intervalEnd)                   (endTime)
+    
+    
+    properties (SetAccess = private)
+        
+        % input from constructor parameters
+        spikeTimes
+        baselineStartTime
+        stimTime
+        endTime
+        samplingRate
+        description  % property that controls baseLineEnd, endOffset
+    end
     
     properties
-        epochParameters
         intervalStart
-        baselineStart
-        endTime
         meanBaseLineRate
     end
     
     properties (Dependent)
-        
-        intervalEnd
         baseLineEnd
+        intervalEnd
+        duration
         
+        spikeCount
         responseIntervalLen
         baselineIntervalLen
         poststimIntervalLen
-        spikeTimes
         isi
         
         baselineSpikeTimes
@@ -50,17 +59,31 @@ classdef SpikeTimeFeature < sa_labs.analysis.entity.Feature
     
     methods
         
-        function obj = SpikeTimeFeature(description, data, parameters)
-            obj = obj@sa_labs.analysis.entity.Feature(description, data);
+        function obj = SpikeTimeFeature(spikeTimes, baselineStartTime, stimTime, samplingRate, endTime, description)
             
-            obj.epochParameters = parameters;
+            if nargin < 6
+                description = struct();
+                description.id = 'SPIKE_TIME_FEATURE';
+                description.baseLineEnd = 0;
+                description.endOffset = 0;
+                obj.description = description;
+            end
+            
             obj.intervalStart = 0;
-            obj.baselineStart = obj.epochParameters.preTime * 1e-3;
-            obj.endTime =  (obj.epochParameters.stimTime + obj.epochParameters.tailTime) * 1e-3;
+            obj.baselineStartTime = baselineStartTime;
+            obj.stimTime = stimTime;
+            obj.endTime = endTime;
+            obj.spikeTimes = spikeTimes;
+            obj.description = description;
+            obj.samplingRate = samplingRate;
         end
         
         function data = get.intervalEnd(obj)
-            data = (obj.epochParameters.stimTime + obj.description.endOffset) * 1e-3;
+            data = obj.stimTime +  obj.description.endOffset;
+        end
+        
+        function data = get.duration(obj)
+            data = (obj.endTime - obj.baselineStartTime);
         end
         
         function data = get.responseIntervalLen(obj)
@@ -68,19 +91,15 @@ classdef SpikeTimeFeature < sa_labs.analysis.entity.Feature
         end
         
         function data = get.baseLineEnd(obj)
-            data = obj.description.baseLineEnd * 1e-3;
+            data = obj.description.baseLineEnd;
         end
         
         function data = get.baselineIntervalLen(obj)
-            data = obj.baseLineEnd - obj.baselineStart;
+            data = obj.baseLineEnd - obj.baselineStartTime;
         end
         
         function data = get.poststimIntervalLen(obj)
             data = obj.endTime - obj.intervalEnd;
-        end
-        
-        function data = get.spikeTimes(obj)
-            data = obj.data;
         end
         
         function data = get.isi(obj)
@@ -135,9 +154,20 @@ classdef SpikeTimeFeature < sa_labs.analysis.entity.Feature
             end
         end
         
+        function data = get.spikeCount(obj)
+            data = length(obj.spikeTimes);
+        end
+        
+        function objs = setMeanBaseLineRate(objs, value)
+            for i = 1 : numel(objs)
+                objs(i).meanBaseLineRate = value;
+            end
+        end
+        
         function data = get.meanBaseLineRate(obj)
             
             if isempty(obj.meanBaseLineRate)
+                disp('[INFO] mean baseline rate is empty; using just base line rate as mean rate')
                 data = obj.baseLineRate;
                 return
             end
@@ -147,7 +177,7 @@ classdef SpikeTimeFeature < sa_labs.analysis.entity.Feature
         % below method requires meanBaseLineRate
         
         function data = get.baseLineSubstractedSpikeCount(obj)
-            data = obj.spikeCount - obj.meanBaselineRate / obj.responseIntervalLen;
+            data = obj.spikeCount - obj.meanBaseLineRate / obj.responseIntervalLen;
         end
         
         function data = get.baseLineSubstractedSpikeRate(obj)
@@ -155,12 +185,71 @@ classdef SpikeTimeFeature < sa_labs.analysis.entity.Feature
         end
         
         function data = get.baseLineSubstractedOnSetSpikeCount400ms(obj)
-            data = obj.onSetSpikeCount400ms - obj.meanBaselineRate / 0.4;
+            data = obj.onSetSpikeCount400ms - obj.meanBaseLineRate / 0.4;
         end
         
         function data = get.baseLineSubstractedOffSetSpikeCount400ms(obj)
-            data = obj.OffSetSpikeCount400ms - obj.meanBaselineRate / 0.4;
+            data = obj.OffSetSpikeCount400ms - obj.meanBaseLineRate / 0.4;
+        end
+    end
+    
+    methods (Static)
+        
+        function psthDescription = parsePSTHInputParameters(varargin)
+            ip = inputParser();
+            ip.PartialMatching = true;
+            ip.KeepUnmatched = true;
+            
+            if ismember({'description'}, {varargin{1 : 2 : end -1}})
+                ip.parse(varargin{:});
+                psthDescription = ip.Unmatched.description;
+            else
+                ip.addParameter('id', 'PSTH_FEATURE', @ischar);
+                ip.addParameter('smoothingWindowPSTH', 0, @isnumeric);
+                ip.addParameter('binWidthPSTH', 0.01, @isnumeric);
+                ip.parse(varargin{:});
+                psthDescription = ip.Results;
+            end
         end
         
+        
+        function [x, count] = getPSTH(spikeTimeFeatures, varargin)
+            
+            import sa_labs.common.spike.features.*;
+            desc = SpikeTimeFeature.parsePSTHInputParameters(varargin{:});
+            
+            smoothingWindowPSTH = desc.smoothingWindowPSTH;
+            binWidthPSTH = desc.binWidthPSTH;
+            
+            duration = unique([spikeTimeFeatures.duration]);
+            
+            if numel(duration) > 1
+                error('cannot get psth for varying response duration')
+            end
+            
+            rate = spikeTimeFeatures(1).samplingRate;
+            preTime = - spikeTimeFeatures(1).baselineStartTime;
+            
+            n = round(binWidthPSTH * rate);
+            durationSteps = duration * rate;
+            
+            bins = 1 : n : durationSteps;
+            spikeTimeSteps = ([spikeTimeFeatures.spikeTimes] + preTime) * rate;
+            count = histc(spikeTimeSteps, bins);
+            
+            if isempty(count)
+                count = zeros(1, length(bins));
+            end
+            
+            if smoothingWindowPSTH
+                smoothingWindowSteps = round(rate * (smoothingWindowPSTH / binWidthPSTH));
+                w = gausswin(smoothingWindowSteps);
+                w = w / sum(w);
+                count = conv(count, w, 'same');
+            end
+            
+            count = count / numel(spikeTimeFeatures) / binWidthPSTH ;
+            x = bins/rate - preTime;
+        end
     end
 end
