82066040a4677cf4a67a92271158041c81f4fbc2
Sathish Narayanan <ragav.sathish@gmail.com>
2017-11-09 17:45:40 +0200

    CleanUp examples, deprecate mht spike detection and Fix plot for curator

--- src/main/matlab/+sa_labs/+analysis/+data_curator/+pre_processor/+cell/formatParameter.m
+++ src/main/matlab/+sa_labs/+analysis/+data_curator/+pre_processor/+cell/formatParameter.m
@@ -1,12 +1,6 @@
 function formatParameter(cellData, parameters)
 % description : Standardize cell data and epoch parameters for analysis. For Example, if the file is from symphony_v1, then format parameter as per new analysis.
 % ---
-for epoch = cellData.epochs
-    number = epoch.get('epochNumber');
-    if ~ isempty(number)
-        remove(epoch.attributes, epochNumber);
-        epoch.attributes('h5EpochNumber') = number;
-    end
-end
+
 end
 
